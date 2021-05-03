aab973ec6975bec587d02c4d42c30e2b0127ba5e
blanketmanatee <ashleyl.welch@gmail.com>
2021-05-03 00:39:22 +0000

    pep

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -10,15 +10,18 @@ def index():
     """ return Hello HBNB! """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ return HBNB """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c():
     """ return C """
     return 'C'
+
 
 @app.route('/python/(<text>)', strict_slashes=False)
 def python():
